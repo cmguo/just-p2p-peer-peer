40d72362b8ca35d156c34ee303d1bb9614bc7409
jeffreywu <jeffreywu@pptv.com>
2011-07-20 06:50:19 +0000

    删除AccessControl, ByteStream, Path, WinFile 修改StdFile，删除其中没有用到的函数
    git-svn-id: http://svn01/repositories/r01/p2p/trunk@199 b3a1389e-2159-5c43-9038-463bf877d0d9

--- io/StdFile.h
+++ io/StdFile.h
@@ -5,117 +5,46 @@
 #ifndef FRAMEWORK_IO_STDFILE_H
 #define FRAMEWORK_IO_STDFILE_H
 
-#ifdef BOOST_WINDOWS_API
-#include "io/AccessControl.h"
-#endif  // BOOST_WINDOWS_API
-
 #include <stdarg.h>
 
 namespace io
 {
-
     class StdFile: private boost::noncopyable
     {
     public:
-        StdFile() :
-          m_handle(NULL)
-          {
-          }
-          ~StdFile()
-          {
-              Close();
-          }
-
-          FILE* GetHandle() const
-          {
-              return m_handle;
-          }
-
-          bool IsOpen() const
-          {
-              return m_handle != NULL;
-          }
-
-          void Close();
-
-          bool IsEOF() const;
-
-          bool Open(const char* path, const char* mode = "r");
-
-          int ReadByte()
-          {
-              return fgetc(m_handle);
-          }
-
-          uint32_t Read(void* buf, uint32_t size);
-
-          bool ReadLine(char* buf, int size);
-
-          bool ReadLine(string& line);
-
-#ifdef _WIN
-          bool ReadLine(wchar_t* buf, int size);
-          bool ReadLine(wstring& line);
-#endif
-
-          uint32_t Write(const void* data, uint32_t size);
-
-          bool Write(const char* str);
-
-          int WriteV(const char* format, va_list argptr);
-
-          int WriteF(const char* format, ...);
-
-          template<typename StructT>
-          bool WriteStruct(const StructT& buffer)
-          {
-              uint32_t size = Write(&buffer, sizeof(StructT));
-              return size == sizeof(StructT);
-          }
-
-          template<typename StructT>
-          bool ReadStruct(StructT& buffer)
-          {
-              uint32_t size = Read(&buffer, sizeof(StructT));
-              return size == sizeof(StructT);
-          }
-
-          bool Seek(long offset, int origin = SEEK_SET)
-          {
-              return 0 == fseek(m_handle, offset, origin);
-          }
-
-          bool Flush();
-
-          bool IsFailed();
-
-    protected:
-        FILE* m_handle;
-    };
-
-    class StdFileReader: public StdFile
-    {
-    public:
-        bool OpenBinary(const char* path);
-
-        bool OpenText(const char* path);
-    };
-
-    class StdFileWriter: public StdFile
-    {
-    public:
-        StdFileWriter()
+        StdFile()
+            : m_handle(NULL)
         {
+
         }
-        ~StdFileWriter();
+
+        ~StdFile()
+        {
+            Close();
+        }
+
+        bool IsOpen() const
+        {
+            return m_handle != NULL;
+        }
 
         void Close();
 
-        bool OpenBinary(const char* path);
+        bool Open(const char* path, const char* mode = "r");
 
-        bool OpenText(const char* path);
+        uint32_t Read(void* buf, uint32_t size);
 
-        static bool WriteBinary(const char* path, const void* data, uint32_t size);
+        uint32_t Write(const void* data, uint32_t size);
+
+        bool Seek(long offset, int origin = SEEK_SET)
+        {
+            return 0 == fseek(m_handle, offset, origin);
+        }
+
+        bool Flush();
+
+    protected:
+        FILE* m_handle;
     };
 
     inline bool StdFile::Flush()
@@ -128,31 +57,6 @@ namespace io
         return EOF != fflush(m_handle);
     }
 
-    inline bool StdFile::IsFailed()
-    {
-        assert(IsOpen());
-        return ferror(m_handle) != 0;
-    }
-    inline int StdFile::WriteV(const char* format, va_list argptr)
-    {
-        assert(format != NULL);
-        assert(IsOpen());
-        if (!IsOpen())
-        {
-            return 0;
-        }
-        return vfprintf(m_handle, format, argptr);
-    }
-    inline bool StdFile::Write(const char* str)
-    {
-        assert(str != NULL);
-        assert(IsOpen());
-        if (!IsOpen())
-        {
-            return false;
-        }
-        return EOF != fputs(str, m_handle);
-    }
     inline uint32_t StdFile::Write(const void* data, uint32_t size)
     {
         assert(data != NULL && size > 0);
@@ -163,6 +67,7 @@ namespace io
         }
         return fwrite(data, size, 1, m_handle) * size;
     }
+
     inline uint32_t StdFile::Read(void* buf, uint32_t size)
     {
         assert(buf != NULL && size > 0);
@@ -172,10 +77,6 @@ namespace io
             return 0;
         }
         return fread(buf, 1, size, m_handle);
-    }
-    inline bool StdFile::IsEOF() const
-    {
-        return 0 != feof(m_handle);
     }
 
     inline void StdFile::Close()
@@ -193,140 +94,10 @@ namespace io
         m_handle = fopen(path, mode);
         if (IsOpen())
         {
-#ifdef _WIN
-            framework::io::AccessControl::AddAccessRights(path, TEXT("Everyone"), GENERIC_ALL);
-#endif
             return true;
         }
         return false;
     }
-
-    inline bool StdFile::ReadLine(char* buf, int size)
-    {
-        assert(buf != NULL && size > 0);
-        assert(IsOpen());
-        if (!IsOpen())
-            return false;
-        if (fgets(buf, size, m_handle) != NULL)
-            return true;
-        return false;
-    }
-
-    inline bool StdFile::ReadLine(string& line)
-    {
-        assert(IsOpen());
-        if (!IsOpen())
-            return false;
-        const uint32_t max_size = 1025;
-        line.clear();
-        while (true)
-        {
-            char str[max_size] = { 0 };
-            if (fgets(str, max_size, m_handle) == NULL)
-            {
-                return false;
-            }
-            string tmp_line = str;
-            line += tmp_line;
-            if (tmp_line.size() < 1024 || (tmp_line.size() == 1024 && tmp_line[tmp_line.size() - 1] == '\n'))
-            {
-                break;
-            }
-        }
-        return true;
-    }
-#ifdef _WIN
-    inline bool StdFile::ReadLine(wchar_t* buf, int size)
-    {
-        assert(buf != NULL && size > 0);
-        assert(IsOpen());
-        if (!IsOpen())
-            return false;
-        if (fgetws(buf, size, m_handle) != NULL)
-            return true;
-        if (IsFailed())
-        {
-            UTIL_DEBUG("fgets failed ");
-        }
-        return false;
-    }
-
-    inline bool StdFile::ReadLine(wstring& line)
-    {
-        assert(IsOpen());
-        if (!IsOpen())
-            return false;
-        const uint32_t max_size = 1024;
-        wchar_t str[max_size + 1] =
-        {   0};
-        if (fgetws(str, max_size, m_handle) == NULL)
-        {
-            if (IsFailed())
-            {
-                UTIL_DEBUG("fgets failed");
-            }
-            return false;
-        }
-        line = str;
-        return true;
-    }
-#endif
-
-    inline int StdFile::WriteF(const char* format, ...)
-    {
-        assert(format != NULL);
-        va_list(args);
-        va_start(args, format);
-        int count = WriteV(format, args);
-        va_end(args);
-        return count;
-    }
-
-    inline bool StdFileReader::OpenBinary(const char* path)
-    {
-        return this->Open(path, ("rb"));
-    }
-
-    inline bool StdFileReader::OpenText(const char* path)
-    {
-        return this->Open(path, ("r"));
-    }
-
-    inline bool StdFileWriter::OpenBinary(const char* path)
-    {
-        return this->Open(path, ("wb"));
-    }
-
-    inline bool StdFileWriter::OpenText(const char* path)
-    {
-        return this->Open(path, ("w"));
-    }
-
-    inline StdFileWriter::~StdFileWriter()
-    {
-        Close();
-    }
-
-    inline void StdFileWriter::Close()
-    {
-        if (IsOpen())
-        {
-            Flush();
-            StdFile::Close();
-        }
-    }
-
-    inline bool StdFileWriter::WriteBinary(const char* path, const void* data, uint32_t size)
-    {
-        StdFileWriter file;
-        if (!file.OpenBinary(path))
-            return false;
-        if (file.Write(data, size) != size)
-            return false;
-        file.Flush();
-        return true;
-    }
-
 }// io
 
 #endif  // FRAMEWORK_IO_STDFILE_H
