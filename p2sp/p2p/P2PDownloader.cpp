fcb75a2297643db6c077c10c3f248493c6f6aeec
jeffreywu <jeffreywu@pptv.com>
2012-05-08 02:26:00 +0000

    非PC平台使用static_pointer_cast. PC平台如果使用static_pointer_cast会有潜在的Crash
    git-svn-id: http://svn01/repositories/r01/p2p/trunk@3465 b3a1389e-2159-5c43-9038-463bf877d0d9

--- p2sp/p2p/P2PDownloader.cpp
+++ p2sp/p2p/P2PDownloader.cpp
@@ -980,7 +980,11 @@ namespace p2sp
             if (peers_.find(packet.end_point) != peers_.end())
             {
                 assert(peers_[packet.end_point]);
+#ifdef BOOST_WINDOWS_API
                 PeerConnection::p peer_connection = boost::dynamic_pointer_cast<PeerConnection>(peers_[packet.end_point]);
+#else
+                PeerConnection::p peer_connection = boost::static_pointer_cast<PeerConnection>(peers_[packet.end_point]);
+#endif
                 assert(peer_connection);
                 if (peer_connection)
                 {
