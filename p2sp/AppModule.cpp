159f64c2672697df320aca1f97de004d775ddb91
jeffreywu <jeffreywu@pptv.com>
2012-06-23 07:20:14 +0000

    revert 3916中Appmodule.cpp   UdpServerHandlePacket.h   以及 UdpServer.cpp 的改动。 至此，3916改动已经全部revert.
    git-svn-id: http://svn01/repositories/r01/p2p/trunk@3950 b3a1389e-2159-5c43-9038-463bf877d0d9

--- p2sp/AppModule.cpp
+++ p2sp/AppModule.cpp
@@ -293,12 +293,6 @@ namespace p2sp
         ((framework::timer::AsioTimerManager &)global_second_timer()).stop();
         ((framework::timer::AsioTimerManager &)global_250ms_timer()).stop();
 
-        if (udp_server_)
-        {
-            udp_server_->Close();
-            udp_server_.reset();
-        }
-
         if (tcp_server_)
         {
             tcp_server_->Stop();
@@ -377,6 +371,12 @@ namespace p2sp
             statistics_collection_controller_->Stop();
         }
         
+        if (udp_server_)
+        {
+            udp_server_->Close();
+            udp_server_.reset();
+        }
+
         LOG(__EVENT, "app", "AppModule has stopped.");
         inst_.reset();
 
