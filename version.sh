3030c4fc036863461f80041c77234ffc7b82309a
jackzhu <jackzhu@pptv.com>
2012-12-13 07:52:05 +0000

    fix android build
    git-svn-id: http://svn01/repositories/r01/p2p/branches/peer4box@5890 b3a1389e-2159-5c43-9038-463bf877d0d9

--- version.sh
+++ version.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-
 src_dir=${PWD}/../../../../src/p2p/peer
 version_dir=${src_dir}/../../ppbox/ppbox
 file_prefix=autopeerversion
 build_version=`LANG=C svn info ${version_dir} | awk -F : '$1 == "Revision" { print $2}'`
 build_version=`echo ${build_version}`
-cp ${src_dir}/${file_prefix}.hpp ${src_dir}/${file_prefix}.h
+old_header=${src_dir}/${file_prefix}.h
+new_header=${src_dir}/${file_prefix}.hbak
+cp ${src_dir}/${file_prefix}.hpp ${new_header}
 
 if [ `uname` = 'Darwin' ]; then
-    sed -i '' "s/\\\$WCREV\\\$/${build_version}/g" ${src_dir}/${file_prefix}.h
+    sed -i '' "s/\\\$WCREV\\\$/${build_version}/g" ${new_header}
 else
-    sed -i "s/\\\$WCREV\\\$/${build_version}/g" ${src_dir}/${file_prefix}.h
+    sed -i "s/\\\$WCREV\\\$/${build_version}/g" ${new_header}
 fi
+
+
+if [ ! -e "$old_header" ];then
+	touch $old_header
+fi
+
+count=`diff $old_header $new_header | wc -l`
+if [ $count -gt 0 ]; then
+	mv $new_header $old_header
+else
+	rm -f $new_header
+fi
+
+
 
